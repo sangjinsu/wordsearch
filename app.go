5d8ae5e51e8ee297edc89c6ee5fe419990178d57
jinsuSang <climbplant39@kakao.com>
2021-07-25 12:26:09 +0900

    read lines by opening file

--- app.go
+++ app.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+type Line struct {
+	number int
+	text   string
+}
+
+type FileData struct {
+	fileName string
+	lines    []Line
+}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -14,26 +26,69 @@ func main() {
 
 	word := os.Args[1]
 	files := os.Args[2:]
+	var fileDataList []FileData
 
-	fmt.Println("찾으려는 단어:", word)
-	PrintAllFiles(files)
+	for _, path := range files {
+		fileDataList = append(fileDataList, FindWordInFiles(word, path)...)
+	}
+
+	for _, fileData := range fileDataList {
+		fmt.Println(fileData.fileName)
+		fmt.Println("=============================================")
+		for _, line := range fileData.lines {
+			fmt.Println("\t", line.number, "\t", line.text)
+		}
+		fmt.Println("=============================================")
+		fmt.Println()
+	}
 }
 
 func GetFileList(path string) ([]string, error) {
 	return filepath.Glob(path)
 }
 
-func PrintAllFiles(filePaths []string) {
-	for _, path := range filePaths {
-		fileList, err := GetFileList(path)
+func FindWordInFiles(word string, filePath string) []FileData {
+	var fileDataList []FileData
+
+	fileList, err := GetFileList(filePath)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. err:", err)
+		return fileDataList
+	}
+
+	for _, name := range fileList {
+		fileDataList = append(fileDataList, FindWordInFile(word, name))
+	}
+
+	return fileDataList
+}
+
+func FindWordInFile(word, fileName string) FileData {
+	fileData := FileData{fileName, []Line{}}
+	file, err := os.Open(fileName)
+	defer func(file *os.File) {
+		err := file.Close()
 		if err != nil {
-			fmt.Println("파일을 찾을 수 없습니다. err:", err)
 			return
 		}
-
-		fmt.Println("찾으려는 파일 리스트")
-		for _, name := range fileList {
-			fmt.Println(name)
-		}
+	}(file)
+	if err != nil {
+		fmt.Println(fileName, "파일을 찾을 수 없습니다. err:", err)
+		return fileData
 	}
+
+	lineNumber := 1
+	scanner := bufio.NewScanner(file)
+
+	wordToLower := strings.ToLower(word)
+	for scanner.Scan() {
+		text := scanner.Text()
+		textToLower := strings.ToLower(text)
+		if strings.Contains(textToLower, wordToLower) {
+			fileData.lines = append(fileData.lines, Line{lineNumber, text})
+		}
+		lineNumber++
+	}
+
+	return fileData
 }
